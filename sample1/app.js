a96dce3eaea9c324e320298aff0ccce971e0d366
伊藤 吉弥 <myon53@gmail.com>
2014-05-04 02:19:15 +0900

    add skelton controller

--- sample1/app.js
+++ sample1/app.js
@@ -1,6 +1,15 @@
+/**
+ * @fileoverview app.js
+ * @name app.js
+ * @author Yoshiya Ito <ito_yoshiya@cyberagnet.co.jp>
+ */
 var express = require('express');
+var controller = require('./route');
 var app = express();
 
 app.use(express.static(__dirname + '/static'));
+
+app.get('/admin',controller.get);
+app.post('/admin', controller.post);
 
 app.listen(8080);
---
+++ sample1/route/index.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview controller
+ * @name index.js
+ * @author Yoshiya Ito <ito_yoshiya@cyberagnet.co.jp>
+ */
+var mongo = require('../lib');
+var async = require('async');
+var _ = require('lodash');
+
+//TODO in the future using webscoket and socketIO and redis
+
+/**
+ * GET method
+ * @method get
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+exports.get = function(req, res, next) {
+};
+
+/**
+ * POST method
+ * @method get
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+exports.post = function(req, res, next) {
+};
+
+/**
+ * DELETE method
+ * @method delete
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+exports.delete = function(req, res, next) {
+};
