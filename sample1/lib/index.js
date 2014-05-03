6b23adb5d2f76941e0f2ca17c6987df2033cf9f3
伊藤 吉弥 <myon53@gmail.com>
2014-05-04 02:08:25 +0900

    add service using mongoskin

---
+++ sample1/lib/index.js
@@ -0,0 +1,67 @@
+/**
+ * @fileoverview mongoDB service 
+ * @name index.js
+ * @author Yoshiya Ito <ito_yoshiya@cyberagnet.co.jp>
+ */
+
+var mongoskin = require('mongoskin');
+var async = require('async');
+var _ = require('lodash');
+
+var db = mongoskin.db("mongodb://localhost:27017/sample1", {native_parser:true});
+
+/**
+ * get all collection names
+ * @method getCollections
+ * @param {Function} callback
+ */
+exports.getCollections = function(callback) {
+
+    //get collection names
+    db.collectionNames(function(err, list) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, {
+            names: _.pluck(list, 'name')
+        });
+    });
+};
+
+/**
+ * get all document of target collections
+ * @method list
+ * @param {String} col
+ * @param {Function} callback
+ */
+exports.list = function(col, callback) {
+
+    //get target collection
+    var collection = db.collection(col);
+
+    collection.find({}).toArray(function(err, result) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, result);
+    });
+};
+
+/**
+ * upsert document
+ * @method upsert
+ * @param {String} col
+ * @param {Object} query
+ * @param {Object} update
+ * @param {Function} callback
+ */
+exports.upsert = function(col, query, update, callback) {
+    var collection = db.collection(col);
+
+    collection.update(query, {$set: update}, {upsert: true, safe: true}, function(err, result) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, result);
+    });
+};
